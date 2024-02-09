633137aa62d110fbd3eb1e850c638c08cfefc494
Menatalla Gomaa <minafathi98@gmail.com>
2024-02-09 16:13:28 +0100

    Header is DONE

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Header from "./components/Header/Header";
+
 function App() {
-  return <span>subscribe Zainkeepscode</span>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
