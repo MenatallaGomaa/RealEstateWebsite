c438a2a14811533c391f14e8b717dcbc6bfae2ed
Menatalla Gomaa <minafathi98@gmail.com>
2024-02-10 14:12:03 +0100

    fixing the Header

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,23 +1,31 @@
 import "./Hero.css";
+import { HiLocationMarker } from "react-icons/hi";
+import CountUp from "react-countup";
 
 const Hero = () => {
   return (
     <section className="hero-wrapper">
       <div className="paddings innerWidth flexCenter hero-container">
         {/* left side */}
-        <div className="flexColStart hero-left">
+        <div className="hero-left">
           <div className="hero-title">
             <div className="orange-circle" />
-            Discover <br />
-            Most Suitable
-            <br /> Property
+            <h1>
+              Discover <br />
+              Most Suitable <br /> Property
+            </h1>
           </div>
-          <div className="flexColStart secondaryText flexhero-des">
-            <span>Find a variety of properties that suit you very easilty</span>
-            <span>Forget all difficulties in finding a residence for you</span>
+          <div className="flexColStart flexhero-des">
+            <span className="secondaryText">
+              Find a variety of properties that suit you very easilty
+            </span>
+            <span className="secondaryText">
+              Forget all difficulties in finding a residence for you
+            </span>
           </div>
 
           <div className="flexCenter search-bar">
+            <HiLocationMarker color="var(--blue)" size={25} />
             <input type="text" />
             <button className="button">Search</button>
           </div>
@@ -25,6 +33,7 @@ const Hero = () => {
           <div className="flexCenter stats">
             <div className="flexColCenter stat">
               <span>
+                <CountUp start={8800} end={9000} duration={4} />
                 <span>+</span>
               </span>
               <span className="secondaryText">Premium Product</span>
@@ -32,6 +41,7 @@ const Hero = () => {
 
             <div className="flexColCenter stat">
               <span>
+                <CountUp start={1950} end={2000} duration={4} />
                 <span>+</span>
               </span>
               <span className="secondaryText">Happy Customer</span>
@@ -39,6 +49,7 @@ const Hero = () => {
 
             <div className="flexColCenter stat">
               <span>
+                <CountUp end={28} />
                 <span>+</span>
               </span>
               <span className="secondaryText">Awards Winning</span>
